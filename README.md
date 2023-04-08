d4bd6331edfdae9ec6f8579e97a5a4c55835510b
alden <17280789+ald3ns@users.noreply.github.com>
2023-04-08 16:14:10 -0400

    Added the rest of the functionality + cleaned up.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Sample Plugin
-Author: **Vector 35 Inc**
+# Copy As Yara
+Author: **alden (birch)**
 
-_This is a short description meant to fit on one line._
+_Copy disassembly nicely formatted for YARA rules._
 
 ## Description:
 This is a longer description meant for a sample plugin that demonstrates the metadata format for Binary Ninja plugins. Note that the [community-plugins repo](https://github.com/Vector35/community-plugins) contains a useful [utility](https://github.com/Vector35/community-plugins/blob/master/generate_plugininfo.py) to validate the plugin.json. Additionally, the [release helper](https://github.com/Vector35/release_helper) plugin is helpful for more easily pushing new releases, incrementing versions, and creating the appropriate GitHub tags.
--- __init__.py
+++ __init__.py
@@ -1,137 +1,148 @@
 import struct
 
-from binaryninja import *
-
 from binaryninja.log import log_error, log_debug, log_alert, log_warn
-from binaryninja.plugin import BackgroundTaskThread, PluginCommand
-from binaryninjaui import UIContext
+from binaryninja.binaryview import BinaryView
+from binaryninja.enums import InstructionTextTokenType
+from binaryninja.plugin import PluginCommand
+from binaryninjaui import UIContext, UIActionContext
+from PySide6.QtGui import QGuiApplication
 
 
-# Stolen from 'snippets' plugin: https://github.com/Vector35/snippets/blob/680e7ef3b87a9aae289dab7673ab221d3e87bac6/__init__.py#L129
-def setup_context():
-    # Get UI context, try currently selected otherwise default to the first one if the snippet widget is selected.
+# Stolen from 'snippets' plugin:
+# https://github.com/Vector35/snippets/blob/680e7ef3b87a9aae289dab7673ab221d3e87bac6/__init__.py#L129
+def setup_context() -> UIActionContext:
+    """
+    Get the current UIContext at the time of the plugin run.
+    :return: UIActionContext object
+    """
     ctx = UIContext.activeContext()
-    dummycontext = {
-        "binaryView": None,
-        "address": None,
-        "function": None,
-        "token": None,
-        "lowLevelILFunction": None,
-        "mediumLevelILFunction": None,
-    }
-    if not ctx:
-        ctx = UIContext.allContexts()[0]
-
-    else:
-        handler = ctx.contentActionHandler()
-        if handler:
-            context = handler.actionContext()
+    handler = ctx.contentActionHandler()
+    if handler:
+        context = handler.actionContext()
 
     return context
 
 
-# Stolen from 'snippets' plugin: https://github.com/Vector35/snippets/blob/680e7ef3b87a9aae289dab7673ab221d3e87bac6/__init__.py#97
-def setupGlobals(context):
-    snippetGlobals = {}
-    snippetGlobals["current_view"] = context.binaryView
-    snippetGlobals["bv"] = context.binaryView
-    snippetGlobals["current_token"] = None
-    snippetGlobals["current_hlil"] = None
-    snippetGlobals["current_mlil"] = None
-    snippetGlobals["current_function"] = None
+# Stolen from 'snippets' plugin:
+# https://github.com/Vector35/snippets/blob/680e7ef3b87a9aae289dab7673ab221d3e87bac6/__init__.py#97
+def setup_globals(context) -> dict:
+    """
+    Get the global variables that are accessible in the python console (current_function, current_selection).
+    :param context:
+    :return:
+    """
+    snippet_globals = {}
+    snippet_globals["current_function"] = None
 
     if context.function:
-        snippetGlobals["current_function"] = context.function
-        snippetGlobals["current_mlil"] = context.function.mlil
-        snippetGlobals["current_hlil"] = context.function.hlil
-        snippetGlobals["current_llil"] = context.function.llil
-        if context.token:
-            # Doubly nested because the first token is a HighlightTokenState
-            snippetGlobals["current_token"] = context.token
-        snippetGlobals["current_basic_block"] = context.function.get_basic_block_at(
-            context.address
-        )
-    else:
-        snippetGlobals["current_basic_block"] = None
+        snippet_globals["current_function"] = context.function
 
-    snippetGlobals["current_address"] = context.address
-    snippetGlobals["here"] = context.address
+    # Get the current_selection
     if context.address is not None and isinstance(context.length, int):
-        snippetGlobals["current_selection"] = (
+        snippet_globals["current_selection"] = (
             context.address,
             context.address + context.length,
         )
     else:
-        snippetGlobals["current_selection"] = None
-    snippetGlobals["uicontext"] = context
-    return snippetGlobals
+        snippet_globals["current_selection"] = None
+
+    return snippet_globals
 
 
-def copy_as_yara(wildcarding: bool = False) -> None:
+def copy_as_yara(bv: BinaryView, wildcarding: bool = False) -> None:
+    """
+
+    :param bv: the binary view provided when plugin is called
+    :param wildcarding: do the wildcard replacing for bytes?
+    :return: None
+    """
     opcodes = []
-    pneumonics = []
+    mnemonics = []
 
     # Call this locally otherwise it will use the context from plugin load time
-    context = setupGlobals(setup_context())
+    context = setup_globals(setup_context())
 
     current_function = context["current_function"]
     current_selection = context["current_selection"]
-    bv = context["bv"]
 
-    for block in current_function:
-        dis_text = block.get_disassembly_text()
+    if current_function is None or current_selection is None:
+        log_error("No function selected!")
+    else:
+        for block in current_function:
+            dis_text = block.get_disassembly_text()
 
-        for idx, inst in enumerate(dis_text):
-            if (
-                inst.address >= current_selection[0]
-                and inst.address < current_selection[1]
-            ):
-                # Get the size of instruction bytes to read based off of addr of next instruction in block
-                # Probably a better way to do this but idk it lol
-                if (idx + 1) < len(dis_text):
-                    machine_code = bv.read(
-                        inst.address, (dis_text[idx + 1].address - inst.address)
-                    ).hex()
-                else:
-                    machine_code = bv.read(
-                        inst.address, (block.end - dis_text[idx].address)
-                    ).hex()
+            for idx, inst in enumerate(dis_text):
+                if (
+                    inst.address >= current_selection[0]
+                    and inst.address < current_selection[1]
+                ):
+                    # Get the size of instruction bytes to read based off of
+                    # addr of next instruction in block.
 
-                # If you don't want addresses in your rule, use this to replace them with wildcards
-                for token in inst.tokens:
-                    # Add in special case for calls to functions?
-                    if str(token) == "call":
-                        machine_code = machine_code[:2] + "?" * len(machine_code)
+                    # Probably a better way to do this but idk it lol
+                    if (idx + 1) < len(dis_text):
+                        machine_code = bv.read(
+                            inst.address, (dis_text[idx + 1].address - inst.address)
+                        ).hex()
+                    else:
+                        machine_code = bv.read(
+                            inst.address, (block.end - dis_text[idx].address)
+                        ).hex()
 
-                    # TODO: See if token confidence produces more accurate results
-                    if token.type == InstructionTextTokenType.PossibleAddressToken:
-                        converted_addr = struct.pack("<I", int(token.text, 16)).hex()
-                        machine_code = machine_code.replace(
-                            converted_addr, "?" * len(converted_addr)
+                    # Replace addresses with wildcards
+                    if wildcarding:
+                        for token in inst.tokens:
+                            # Add in special case for calls to functions?
+                            if str(token) == "call":
+                                machine_code = machine_code[:2] + "?" * len(
+                                    machine_code
+                                )
+
+                            # TODO: See if token confidence produces more accurate results
+                            if (
+                                token.type
+                                == InstructionTextTokenType.PossibleAddressToken
+                            ):
+                                # Swap endianness
+                                converted_addr = struct.pack(
+                                    "<I", int(token.text, 16)
+                                ).hex()
+                                machine_code = machine_code.replace(
+                                    converted_addr, "?" * len(converted_addr)
+                                )
+
+                    opcodes.append(
+                        " ".join(
+                            [
+                                machine_code[i : i + 2]
+                                for i in range(0, len(machine_code), 2)
+                            ]
                         )
-
-                opcodes.append(
-                    " ".join(
-                        [
-                            machine_code[i : i + 2]
-                            for i in range(0, len(machine_code), 2)
-                        ]
                     )
-                )
-                pneumonics.append(str(inst))
+                    mnemonics.append(str(inst))
 
-    # Print out pretty data :)
+    # Build the pretty yara text
     padding = max([len(i) for i in opcodes]) + 1
     to_return = ""
-    for op, pnu in zip(opcodes, pneumonics):
-        # to_return += op + " " * (padding - len(op)) + " // " + pnu + "\n"
+    for op, pnu in zip(opcodes, mnemonics):
         to_return += f"{op}{' ' * (padding - len(op))} // {pnu} \n"
 
-    print(to_return)
+    # Copy to the clipboard
+    clip = QGuiApplication.clipboard()
+    clip.setText(f"{to_return}")
 
 
-def run(bv):
-    copy_as_yara()
+def run(bv: BinaryView) -> None:
+    copy_as_yara(bv)
 
 
-PluginCommand.register("Copy As Yara", "Wildcarding", run)
+def run_wildcard(bv: BinaryView) -> None:
+    copy_as_yara(bv, True)
+
+
+PluginCommand.register("Copy for YARA\\Normal", "Copy the disassembly as is.", run)
+PluginCommand.register(
+    "Copy for YARA\\Address Wildcards",
+    "Replace addresses with wildcards.",
+    run_wildcard,
+)
